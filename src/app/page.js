5c0236540dd98da196a96b342e6138efa0e1ebaa
MARIA <cg3023141@labA205m17.IFSP>
2024-10-02 22:15:01 -0300

    adding images

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Image from "next/image"; 
 
 export default function Home() {
   return (
@@ -8,6 +9,14 @@ export default function Home() {
 
       <main className="p-5">
         <h1 className="text-3xl">Force Ipsum</h1>
+
+        <Image 
+          src={'/img/react.png'}
+          width={500}
+          height={500}
+          alt="logo-react"
+        />
+
         <p>
         Did you hear that? They've shut down the main reactor. We'll be destroyed for sure. This is madness! We're doomed! There'll be no escape for the Princess this time. What's that? Artoo! Artoo-Detoo, where are you? At last! Where have you been? They're heading in this direction. What are we going to do? We'll be sent to the spice mine of Kessel or smashed into who knows what! Wait a minute, where are you going? The Death Star plans are not in the main computer. Where are those transmissions you intercepted? What have you done with those plans? We intercepted no transmissions. Aaah., This is a consular ship.
         </p>
