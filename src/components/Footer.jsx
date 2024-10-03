72d37cc8e81dbb802e80c55d060a6f0617aaa914
MARIA <cg3023141@labA205m17.IFSP>
2024-10-02 22:09:09 -0300

    starting with react and next

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <div className="h-[200px] bg-neutral-800 text-white flex justify-center items-center"/*{style.footer}*/>
+            <p>Copyright 2024 © Maria Eduarda de Faria</p>
+        </div>
+    )
+}
+
+/*const style = {
+    footer: "h-[200px] bg-neutral-800 text-white flex justify-center items-center"
+}*/
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { BsFileBarGraphFill as LogoIFFinance } from "react-icons/bs";
+
+export default function Header() {
+    return (
+    <div className="flex bg-black h-14 dark:bg-neutral-900 items-center p-5">
+        <LogoIFFinance className="text-orange-700 text-4xl"/>
+    </div>
+    )
+}
